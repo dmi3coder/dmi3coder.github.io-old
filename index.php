a0cece30a2224efcbff4b6376d7a6e21a7845ffd
dmi3coder <dmi3coder@gmail.com>
2016-04-01 16:49:22 +0300

    Meta tags fixes

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html><head>
         <meta charset="utf-8">
+        <title>Dmitriy Chaban dmi3coder personal website & portfolio</title>
+        <meta name="description" content="dmi3coder, android, java, akka, play, framework, startup"/>
+        <meta name="keywords" content="dmi3coder, android, java, akka, play, framework, startup"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
         <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
