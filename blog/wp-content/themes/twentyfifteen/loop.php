0f7c7ced29240da1a74a10b9d41648563adff055
dmi3coder <dmi3coder@gmail.com>
2016-04-22 23:14:14 +0300

    Content fixed and filled

--- blog/wp-content/themes/twentyfifteen/loop.php
+++ blog/wp-content/themes/twentyfifteen/loop.php
@@ -1,34 +1,26 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-			</a>
-		<?php endif; ?>
-		<!-- /post thumbnail -->
-
-		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
-
-		<!-- post details -->
-		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-		<!-- /post details -->
-
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-
-		<?php edit_post_link(); ?>
-
-	</article>
-	<!-- /article -->
+	<div class="section">
+		<div class="container">
+			<div class="row"><div class="col-md-6 hidden-sm hidden-xs">
+					<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+							<?php the_post_thumbnail(array(420,420),"class=img-responsive img-rounded"); ?>
+						</a>
+					<?php endif; ?>
+<!--					<img src="/images/exp_dmi3cafe.png" class="img-responsive img-rounded">-->
+				</div>                    <div class="col-md-6">
+					<h2>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+					</h2>
+					<h3>Backend & Mobile Android developer</h3>
+					<p>		<?php the_content() // Build your custom callback length in functions.php ?>
+                    </p>
+                    <p class="text-right"> <b class="date text-right"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></b></p>
+					<a class="btn btn-primary right " style="float:right" href="http://fourplaces.com/"><span class="badge"><?php echo $post->comment_count?></span> More</a>
+                    <?php edit_post_link(); ?>
+				</div>                </div>
+		</div>
+	</div>
 
 <?php endwhile; ?>
 
