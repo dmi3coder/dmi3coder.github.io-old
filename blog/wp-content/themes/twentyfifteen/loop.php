c50d720f6c97c98729d4d413570e9515e93ebdd5
dmi3coder <dmi3coder@gmail.com>
2016-04-23 17:49:52 +0300

    loop image orientation fix

--- blog/wp-content/themes/twentyfifteen/loop.php
+++ blog/wp-content/themes/twentyfifteen/loop.php
@@ -1,14 +1,15 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 	<div class="section">
 		<div class="container">
-			<div class="row"><div class="col-md-6">
-					<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+			<div class="row">
+				<?php if ( has_post_thumbnail() & $post->ID%2 == 1) : // Check if thumbnail exists ?>
+				<div class="col-md-6">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 							<?php the_post_thumbnail(array(420,420),"class=img-responsive img-rounded"); ?>
 						</a>
-					<?php endif; ?>
-<!--					<img src="/images/exp_dmi3cafe.png" class="img-responsive img-rounded">-->
-				</div>                    <div class="col-md-6">
+				</div>
+				<?php endif; ?>
+				<div class="col-md-6">
 					<h2>
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 					</h2>
@@ -18,7 +19,15 @@
                     <p class="text-right"> <b class="date text-right"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></b></p>
 					<a class="btn btn-primary right " style="float:right" href="http://fourplaces.com/"><span class="badge"><?php echo $post->comment_count?></span> More</a>
                     <?php edit_post_link(); ?>
-				</div>                </div>
+				</div>
+				<?php if ( has_post_thumbnail() & $post->ID%2 != 1) : // Check if thumbnail exists ?>
+					<div class="col-md-6">
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+							<?php the_post_thumbnail(array(420,420),"class=img-responsive img-rounded"); ?>
+						</a>
+					</div>
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
 
