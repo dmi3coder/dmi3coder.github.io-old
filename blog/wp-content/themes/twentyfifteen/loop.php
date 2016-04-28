7c81c093bbd42025488e1cc88c6f70597d931470
dmi3coder <dmi3coder@gmail.com>
2016-04-28 21:09:32 +0300

    Category & button fixes

--- blog/wp-content/themes/twentyfifteen/loop.php
+++ blog/wp-content/themes/twentyfifteen/loop.php
@@ -1,7 +1,7 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 	<div class="section">
 		<div class="container">
-			<div class="row">
+			<div class="row" >
 				<?php if ( has_post_thumbnail() & $post->ID%2 == 1) : // Check if thumbnail exists ?>
 				<div class="col-md-6">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
@@ -11,13 +11,13 @@
 				<?php endif; ?>
 				<div class="col-md-6">
 					<h2>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+						<?php the_title(); ?>
 					</h2>
-					<h3>Backend & Mobile Android developer</h3>
+					<h4><?php _e( '', 'html5blank' ); the_category(', ');?></h4>
 					<p>		<?php the_content() // Build your custom callback length in functions.php ?>
                     </p>
                     <p class="text-right"> <b class="date text-right"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></b></p>
-					<a class="btn btn-primary right " style="float:right" href="http://fourplaces.com/"><span class="badge"><?php echo $post->comment_count?></span> More</a>
+					<a class="btn btn-default right " style="float:right" href="<?php the_permalink(); ?>"><span class="badge"><?php echo $post->comment_count?></span> More</a>
                     <?php edit_post_link(); ?>
 				</div>
 				<?php if ( has_post_thumbnail() & $post->ID%2 != 1) : // Check if thumbnail exists ?>
